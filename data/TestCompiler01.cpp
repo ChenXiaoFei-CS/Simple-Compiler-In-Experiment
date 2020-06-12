3306edb8d66bac5440f6cda62c27c0b72c37b3b1
ChenXiaoFei-CS <53506016+ChenXiaoFei-CS@users.noreply.github.com>
2020-06-12 20:05:57 +0800

    Create TestCompiler01.cpp

---
+++ data/TestCompiler01.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+int add(int a,int b){
+	return 2;
+}
+
+int main(int argc,char* argv)
+{
+	int a_t1 = 5,_d = 2;
+	_d = 3 + add(a_t1+_d,_d);
+	char m = 'c';
+	1E-2;
+	double _23m = 10.98;
+	const char* str = "Attr string";
+	str[3] = '5';
+	for(int i=0;i<5;i++){
+		// printf("%d",i);
+		_d += i;
+	}
+	switch(m){
+		case 'c':
+			(_23m = 2);
+			break;
+		default:
+			_23m = 5E-17;
+			break;
+	}
+	if((int)m > 91){
+		a_t1 += _d;
+		_d *= 5.0;
+	}
+	else{
+		int a_t1 = 5;
+		int c = 3;
+	}
+    return 0;
+}
